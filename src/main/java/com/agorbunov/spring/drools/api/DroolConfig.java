f6815ea6533f286bccdb24a93c6aa93ccad4df21
Андрей <andrunov@gmail.com>
2024-06-02 10:48:08 +0300

    use variables

--- src/main/java/com/agorbunov/spring/drools/api/DroolConfig.java
+++ src/main/java/com/agorbunov/spring/drools/api/DroolConfig.java
@@ -23,6 +23,7 @@ public class DroolConfig {
 	private KieFileSystem getKieFileSystem() throws IOException {
 		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
 		kieFileSystem.write(ResourceFactory.newClassPathResource("rates/prog_1_24_01.xls"));
+		kieFileSystem.write(ResourceFactory.newClassPathResource("rates/prog_1_24_02.xls"));
 		return kieFileSystem;
 
 	}
