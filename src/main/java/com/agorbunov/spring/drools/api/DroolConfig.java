dbc04841922b89275756ff62d00b5aa2ecef632f
Андрей <andrunov@gmail.com>
2024-06-06 18:26:38 +0300

    tables update 3

--- src/main/java/com/agorbunov/spring/drools/api/DroolConfig.java
+++ src/main/java/com/agorbunov/spring/drools/api/DroolConfig.java
@@ -22,8 +22,8 @@ public class DroolConfig {
 
 	private KieFileSystem getKieFileSystem() throws IOException {
 		KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-		kieFileSystem.write(ResourceFactory.newClassPathResource("Ставки/Ипотека/prog_1_24_01.xls"));
-		kieFileSystem.write(ResourceFactory.newClassPathResource("Ставки/Ипотека/prog_1_24_02.xls"));
+		kieFileSystem.write(ResourceFactory.newClassPathResource("Ставки/Потребы/prog_1_24_01.xls"));
+		kieFileSystem.write(ResourceFactory.newClassPathResource("Ставки/Потребы/prog_1_24_02.xls"));
 		return kieFileSystem;
 
 	}
